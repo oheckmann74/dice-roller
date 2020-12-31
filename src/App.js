f9641d7c8432e7af39671ee6aa18a0528c6e19d9
Oliver Heckmann <oliver.heckmann@gmail.com>
2020-12-30 19:20:20 -0800

    initial setup

--- src/App.js
+++ src/App.js
@@ -2,42 +2,63 @@ import './App.css';
 import React, { useState } from 'react';
 
 
-function DiceSelector() {
+function DiceRoller() {
   const [dice, setDice] = useState(6);
 
   const handleChange = ({target}) => {
-      setDice(target.dice);
+      setDice(target.value);
   }
 
+  const [result, setResult] = useState([]);
+
   const rollDice = () => {
-      let result = [];
-      for (let index = 0; index < dice; index++) {
-          result[index] = Math.floor(Math.random()*6)+1
-      }
-//        result = result.sort()
-      alert(result);
+    let rresult = [0, 0, 0, 0, 0, 0]
+    for (let index = 0; index < dice; index++) {
+        let roll = Math.floor(Math.random()*6)+1;
+        rresult[roll-1]++;
+    }
+    setResult(rresult);
   }
+
+
+
+  const reset = () => {
+    setResult([]);
+  }
+
+  let resultJSX;
+  if (result.length > 0) {
+    resultJSX = result.map((count, i) => <p>{i+1}: {count}x     {i+1}+: {result.slice(i).reduce((a, b) => a + b, 0)}x</p>)
+  } else {
+    resultJSX = "";
+  }
+
+
 
   return (
       <div>
           <input type="text" value={dice} className='dice-number' onChange={handleChange} />
           <button onClick={rollDice}>Roll</button>
           <div class="slidecontainer">
-            <input type="range" min="1" max="20" value={dice} class="slider" id="myRange" />
+            <input type="range" min="1" max="20" value={dice} className="slider" onChange={handleChange} />
           </div>
+          <div>
+            {resultJSX}
+          </div>
+          <button onClick={reset}>Reset</button>
       </div>
   )
 }
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Dice Roller
-      </header>
-      <DiceSelector />
-      <p> by Oliver </p>
-    </div>
+        Dice Roller App
+        <DiceRoller />
+       </header>
+     </div>
   );
 }
 
